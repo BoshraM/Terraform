d8bddc6febe6d5c163b52edf37238021ee4dda26
cyf <gbboshra@gmail.com>
2024-02-25 01:35:11 +0000

    add security goupe

--- basic/main.tf
+++ basic/main.tf
@@ -7,13 +7,32 @@
 # }
 
 resource "aws_instance" "ec2-terraform" {
-  ami           = "ami-0171207a7acd2a570"
-  instance_type = "t2.micro"
+  ami             = "ami-0171207a7acd2a570"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.webtrafic.name]
   tags = {
-    Name = var.ec2name
+    Name = "ec2-terrafrom"
   }
 }
 
 resource "aws_eip" "eip" {
   instance = aws_instance.ec2-terraform.id
+}
+
+resource "aws_security_group" "webtrafic" {
+  name = "allow HTTPS"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
