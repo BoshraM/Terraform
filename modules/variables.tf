0d3791dc7297f50c2454656b300a359677f506d2
cyf <gbboshra@gmail.com>
2024-03-05 23:59:22 +0000

    copy variable to module

---
+++ modules/variables.tf
@@ -0,0 +1,60 @@
+variable "vpcname" {
+  type    = string
+  default = "myvpc"
+}
+
+variable "ec2name" {
+  type    = string
+  default = "myec2"
+}
+
+variable "sshport" {
+  type    = number
+  default = 22
+}
+
+variable "enabled" {
+  default = true
+}
+
+variable "mylist" {
+  type    = list(string)
+  default = ["value1", "value2"]
+}
+
+variable "mymap" {
+  type = map(any)
+  default = {
+    key1 = "value1"
+    key1 = "value2"
+  }
+}
+
+# variable "inputname" {
+#   type        = string
+#   description = "set the name of the vpc"
+# }
+
+variable "mytuple" {
+  type    = tuple([string, number, string])
+  default = ["cat", 1, "dog"]
+}
+
+variable "myobject" {
+  type = object({ name = string, port = list(number) })
+  default = {
+    name = "myvpc"
+    port = [22, 25, 80]
+  }
+}
+
+
+variable "ingressrules" {
+  type    = list(number)
+  default = [80, 443]
+}
+
+variable "egressrules" {
+  type    = list(number)
+  default = [80, 443, 25, 3306, 53, 8080]
+}
