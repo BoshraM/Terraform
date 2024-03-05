0c0cc8063f74f40fd1bd222213b6b3258cf9869f
cyf <gbboshra@gmail.com>
2024-03-05 23:56:49 +0000

    seperate security group

---
+++ challenge2/sg/sg.tf
@@ -0,0 +1,36 @@
+
+variable "ingressrules" {
+  type    = list(number)
+  default = [80, 443]
+}
+
+variable "egressrules" {
+  type    = list(number)
+  default = [80, 443, 25, 3306, 53, 8080]
+}
+
+resource "aws_security_group" "webtrafic" {
+  name = "Allow Web Traffic"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    iterator = port
+    for_each = var.egressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
